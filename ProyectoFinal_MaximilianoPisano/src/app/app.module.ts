5d81af91525dbd51848e9544f18d9bb98a6f5fef
maxipisano11 <maxi.e.pisano@gmail.com>
2023-03-12 22:19:31 -0300

    Maquetado en Angular

--- ProyectoFinal_MaximilianoPisano/src/app/app.module.ts
+++ ProyectoFinal_MaximilianoPisano/src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { MainComponent } from './components/main/main.component';
+import { AboutComponent } from './components/about/about.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { ProyectsComponent } from './components/proyects/proyects.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BannerComponent,
+    MainComponent,
+    AboutComponent,
+    ExperienciaComponent,
+    SkillsComponent,
+    EducacionComponent,
+    ProyectsComponent,
+    FooterComponent,
+    LoginComponent,
+    ProyectosComponent
   ],
   imports: [
     BrowserModule,
